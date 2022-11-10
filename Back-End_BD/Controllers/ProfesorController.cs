c0a7f3ee3443f4e080c669dee6a86a6a2bf7a39c
19002248 <l.gis.cas@gmail.com>
2022-11-10 13:38:38 -0600

    Agregar archivos de proyecto.

---
+++ Back-End_BD/Controllers/ProfesorController.cs
@@ -0,0 +1,90 @@
+﻿using Back_End_BD.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Back_End_BD.Controllers
+{
+    public class ProfesorController : Controller
+    {
+        // GET: Profesor
+
+        //Index
+        public ActionResult Index()
+        {
+            using (AlumnoDbContext dbAlumnos = new AlumnoDbContext())
+            {
+                return View(dbAlumnos.Profe.ToList());
+            }
+        }
+        //Detalles
+        public ActionResult Details(int? id)
+        {
+            using (AlumnoDbContext dbAlumnos = new AlumnoDbContext())
+            {
+                ProfesoresModel prof = dbAlumnos.Profe.Find(id);
+                if (prof == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(prof);
+            }
+        }
+        //Agregar uno nuevo
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(ProfesoresModel prof)
+        {
+            using (AlumnoDbContext dbAlumnos = new AlumnoDbContext())
+            {
+                dbAlumnos.Profe.Add(prof);
+                dbAlumnos.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        //Editar
+        public ActionResult Edit(int? id)
+        {
+            using (AlumnoDbContext dbAlumnos = new AlumnoDbContext())
+            {
+                return View(dbAlumnos.Profe.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+        [HttpPost]
+        public ActionResult Edit(ProfesoresModel prof)
+        {
+            using (AlumnoDbContext dbAlumnos = new AlumnoDbContext())
+            {
+                dbAlumnos.Entry(prof).State = EntityState.Modified;
+                dbAlumnos.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        //Borrar
+        public ActionResult Delete(int? id)
+        {
+            using (AlumnoDbContext dbAlumnos = new AlumnoDbContext())
+            {
+                return View(dbAlumnos.Profe.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+        [HttpPost]
+        public ActionResult Delete(ProfesoresModel prof, int? id)
+        {
+            using (AlumnoDbContext dbAlumnos = new AlumnoDbContext())
+            {
+                ProfesoresModel pro = dbAlumnos.Profe.Where(x => x.Id == id).FirstOrDefault();
+                dbAlumnos.Profe.Remove(pro);
+                dbAlumnos.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Back-End_BD/Models/ProfesoresModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Back_End_BD.Models
+{
+    public class ProfesoresModel
+    {
+        public int Id { get; set; }
+        public int Matricula { get; set; } 
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Correo { get; set; }
+        public int Edad { get; set; }  
+
+    }
+}
